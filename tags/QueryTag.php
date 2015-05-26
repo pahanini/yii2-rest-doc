ac1ba10891bc5d3656d1a80828de55e4f81aa45d
pahanini <pahanini@gmail.com>
2015-05-26 09:13:21 +0500

    Fix query tag

--- tags/QueryTag.php
+++ tags/QueryTag.php
@@ -16,14 +16,13 @@ class QueryTag extends Tag
     public function setContent($content)
     {
         Tag::setContent($content);
-
         $parts = preg_split('/\s+/Su', $this->description, 2);
         $tmp = explode('=', array_shift($parts));
         if (count($tmp) == 2) {
             $this->defaultValue = $tmp[1];
             $this->variableName = $tmp[0];
         } else {
-            array_unshift($parts, $tmp);
+            $this->variableName = $tmp[0];
         }
         $this->setDescription(join(' ', str_replace("\n", " ", $parts)));
 
